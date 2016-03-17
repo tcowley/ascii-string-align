f5440d3dd7dfe1d6ea391b5cbbfd07a89d3d116d
Trevor Cowley <tcowley@gmail.com>
2016-03-17 13:33:14 -0700

    small test

--- test/index.js
+++ test/index.js
@@ -1,7 +1,7 @@
 var test = require('tape-catch');
 var stringpad = require('../index.js');
 
-test('test empty string with alignment', function (t) {
+test('test empty strings with various alignments', function (t) {
     t.plan(4);
     
     ['left', 'right', 'center', 'justify'].forEach(function(alignment) {
@@ -21,7 +21,6 @@ test('test short strings with various alignments', function (t) {
     ['left', 'right', 'center', 'justify'].forEach(function(alignment, i) {
         var a = 'A B C';
         var b = stringpad(a, 10, alignment);
-        //console.log('[' + b + ']', '[' + c[i] + ']' );
         t.equal(b, c[i], "short string padded to specified width with alignment '" + alignment + "'");
     });
    
@@ -33,7 +32,6 @@ test('test long strings with various alignments', function (t) {
     ['left', 'right', 'center', 'justify'].forEach(function(alignment, i) {
         var a = 'A B C D E F G H I J';
         var b = stringpad(a, 10, alignment);
-        //console.log('[' + b + ']', '[' + a + ']' );
         t.equal(b, a, "string longer than specified width is returned as-is for alignment '" + alignment + "'");
     });
     
