ed944e59981f9f1d2fd466ced7a2ead48b7ee0f1
Trevor Cowley <tcowley@gmail.com>
2016-03-16 20:58:44 -0700

    init repo

---
+++ index.js
@@ -0,0 +1,41 @@
+module.exports = function stringPad(str, width, align) {
+    var spaces = (new Array(width)).join(' ');
+    var paddedStr = '';
+    var right;
+    var left;
+    var words;
+    var padding;
+    
+    switch (align) {
+    case 'right':
+        paddedStr = (spaces + str).slice( -width );
+        break;
+    case 'center':
+        left = Math.ceil((width - str.length)/2);
+        right = width - str.length - left;
+        paddedStr = (spaces + str).slice(-(str.length + left));
+        paddedStr = (paddedStr + spaces).slice(0, width);
+        break;
+    case 'justify':
+        words = str.split(' ');
+        if (words.length < 2) {
+            paddedStr = (spaces).slice(0, width);
+        }
+        else {
+            padding = spaces.slice(0, width - str.length).split('');
+            while (padding.length && padding.length < width) {
+                words = words.map(function(word, i) {
+                    return i && padding.length ? padding.shift() + word : word;
+                });
+            }
+            paddedStr = words.join(' ');
+        }
+        break;
+    default: // 'left'
+        paddedStr = (str + spaces).slice(0, Math.max(str.length, width));
+    }
+    
+    return paddedStr;
+};
+
+
---
+++ test/index.js
@@ -0,0 +1,29 @@
+var test = require('tape-catch');
+var stringpad = require('../index.js');
+
+test('test string length', function (t) {
+    var a = 'aaa                   ';
+    var b;
+    t.plan(2);
+    
+    b = stringpad(a.slice(0,3), 10);
+    t.equal(b, a.slice(0,10), "string shorter than specified width is padded to width");
+
+    b = stringpad(a.slice(0,10), 2);
+    t.equal(b, a.slice(0,10), "string longer than specified width is returned as-is");
+});
+
+
+
+test('test empty string with alignment', function (t) {
+    var a = '';
+    var b;
+    var c = (new Array(10)).join(' ');
+    t.plan(4);
+    
+    ['left', 'right', 'center', 'justify'].forEach(function(alignment) {
+        b = stringpad(a, 10, alignment);
+        t.equal(b, c, "empty string padded to specified to width with align " + alignment);
+    });
+
+});
